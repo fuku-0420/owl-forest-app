6996f182216f78a21dfe049aba5fee55bfc8c8cd
fuku-0420 <8639uverworld@gmail.com>
2025-10-05 08:43:34 +0900

    修正: テストファイルのURLヘルパーを正しい形式に変更

--- test/controllers/owls_controller_test.rb
+++ test/controllers/owls_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class OwlsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get owls_index_url
+    get owls_url  # owls_index_url → owls_url に変更
     assert_response :success
   end
 
   test "should get show" do
-    get owls_show_url
+    get owl_url(owls(:one))  # owls_show_url → owl_url に変更
     assert_response :success
   end
 end
