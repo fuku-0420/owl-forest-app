81dfffaa7513d0a7e9cc94dd981828d0dc87aff5
fuku-0420 <8639uverworld@gmail.com>
2025-10-06 15:31:31 +0900

    test: seeds.rbを追加してフクちゃんデータを保証、表示テスト

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Owl.find_or_create_by(name: "フクちゃん") do |owl|
+  owl.species = "学習サポートフクロウ"
+  owl.age = 12
+  owl.habitat = "RUNTEQ 知識の森"
+  owl.description = ""
+end
