0b8d0847c98c2dc4906c2a93d963e9240245f92c
fuku-0420 <8639uverworld@gmail.com>
2025-10-07 14:45:45 +0900

    Enable serving static assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || true
+  config.assets.compile = true
   config.active_storage.service = :local
   config.assume_ssl = true
   config.force_ssl = true
