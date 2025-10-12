7a696ec9027f5f66042a5099cd4f86e75937f571
fuku-0420 <8639uverworld@gmail.com>
2025-10-12 23:22:38 +0900

    de再修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-Owl.find_or_create_by(name: "フクちゃん") do |owl|
-  owl.species = "学習サポートフクロウ"
-  owl.age = 12
-  owl.habitat = "RUNTEQ 知識の森"
-  owl.description = ""
+Owl.destroy_all  # ← すべて削除（リセット）
+
+Owl.find_or_create_by(name: "フクちゃん") do |owl1|
+  owl1.species = "学習サポートフクロウ"
+  owl1.age = 12
+  owl1.habitat = "RUNTEQ 知識の森"
+  owl1.description = "..."
 end
