05c86ab5a0378a53d337d3656b1a98ed73acfa98
fuku-0420 <8639uverworld@gmail.com>
2026-01-29 18:11:48 +0900

    Lintエラーを修正し、コードスタイルを調整

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
   end
 
   # ユーザー投稿
-  resources :advice_suggestions, only: [:index, :new, :create, :show]
-  resources :advices, only: [:show]
+  resources :advice_suggestions, only: [ :index, :new, :create, :show ]
+  resources :advices, only: [ :show ]
   resources :favorites, only: [ :index, :create, :destroy ]
 
   # 🔧 ユーザー設定（ハブ）
