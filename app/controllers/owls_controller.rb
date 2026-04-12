748ffba3746589ea40527007b3973a674ab8425d
fuku-0420 <8639uverworld@gmail.com>
2026-04-13 06:27:45 +0900

    カテゴリ構成とアドバイス内容を改善

--- app/controllers/owls_controller.rb
+++ app/controllers/owls_controller.rb
@@ -1,7 +1,21 @@
 class OwlsController < ApplicationController
+  CATEGORY_ORDER = {
+    "入門" => 0,
+    "Rails" => 1,
+    "SQL" => 2,
+    "設計" => 3,
+    "開発" => 4,
+    "エラー" => 5,
+    "学習" => 6,
+    "モチベ" => 7,
+    "みんなのお悩み解決" => 8
+  }.freeze
+
   def index
     @owls = Owl.all
-    @categories = Category.includes(:advices).order(:id)
+    @categories = Category.includes(:advices)
+                          .to_a
+                          .sort_by { |category| CATEGORY_ORDER.fetch(category.name, 999) }
 
     @categories_json = @categories.as_json(
       only: [ :id, :name ],
